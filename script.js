b92d0d469f7c46dab549e09940190f9bc9111977
usmanbajwa878 <47910818+usmanbajwa878@users.noreply.github.com>
2019-03-03 15:20:52 +0500

    2nd

---
+++ script.js
@@ -0,0 +1,71 @@
+
+$(document).ready(function() {
+  
+$('.navbar-toggler').click(function(){
+
+
+$('.navbar-toggler').toggleClass('change');
+
+
+
+})
+
+$(window).scroll(function() {
+	let position=$(this).scrollTop();
+	console.log(position);
+
+	if(position>=974){
+$('.navbar').addClass('navbar-background');
+$('.navbar').addClass('fixed-top');
+
+	}
+
+	else{
+
+		$('.navbar').removeClass('navbar-background');
+$('.navbar').removeClass('fixed-top');
+	}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.parent-container').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+
+gallery:{
+
+enabled:true
+
+}
+
+  // other options
+
+
+})
+
+
+
+  });
+
+
+
+
+
+
+
+
---
+++ scriptjs
@@ -0,0 +1,5 @@
+$('.parent-container').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image'
+  // other options
+});
